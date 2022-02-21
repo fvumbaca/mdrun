69206265a7da1f31d442e034903f0d06e171aa96
Frank Vumbaca <fvumbaca@outlook.com>
2022-02-21 02:46:48 -0500

    Updated rendering golden from basic

--- internal/documents/html.go
+++ internal/documents/html.go
@@ -1,7 +1,9 @@
 package documents
 
 import (
+	"fmt"
 	"io"
+	"strconv"
 	"strings"
 
 	bf "github.com/russross/blackfriday/v2"
@@ -14,42 +16,57 @@ func RenderHTML(w io.Writer, doc *Document) {
 }
 
 var tags = map[bf.NodeType][]string{
-	bf.Document:       {"", ""},
-	bf.BlockQuote:     {"", ""},
-	bf.List:           {"", ""},
-	bf.Item:           {"", ""},
-	bf.Paragraph:      {"", ""},
-	bf.Heading:        {"", ""},
-	bf.HorizontalRule: {"", ""},
-	bf.Emph:           {"", ""},
-	bf.Strong:         {"", ""},
-	bf.Del:            {"", ""},
-	bf.Link:           {"", ""},
-	bf.Image:          {"", ""},
-	bf.Text:           {"", ""},
-	bf.HTMLBlock:      {"", ""},
-	bf.CodeBlock:      {"", ""},
-	bf.Softbreak:      {"", ""},
-	bf.Hardbreak:      {"", ""},
-	bf.Code:           {"", ""},
-	bf.HTMLSpan:       {"", ""},
-	bf.Table:          {"", ""},
-	bf.TableCell:      {"", ""},
-	bf.TableHead:      {"", ""},
-	bf.TableBody:      {"", ""},
-	bf.TableRow:       {"", ""},
+	// bf.List:           {"", ""},
+	// bf.Item:           {"", ""},
+	// bf.HorizontalRule: {"", ""},
+	// bf.Emph:           {"", ""},
+	// bf.Strong:         {"", ""},
+	// bf.Del:            {"", ""},
+	// bf.Link:           {"", ""},
+	// bf.Image:          {"", ""},
+	// bf.HTMLBlock:      {"", ""},
+	// bf.Softbreak:      {"", ""},
+	// bf.Hardbreak:      {"", ""},
+	// bf.Code:           {"", ""},
+	// bf.HTMLSpan:       {"", ""},
+	// bf.Table:          {"", ""},
+	// bf.TableCell:      {"", ""},
+	// bf.TableHead:      {"", ""},
+	// bf.TableBody:      {"", ""},
+	// bf.TableRow:       {"", ""},
 }
 
 func renderNode(w io.Writer, node *bf.Node, entering bool) bf.WalkStatus {
 	var ident = 0
 	io.WriteString(w, strings.Repeat("  ", ident))
 	switch node.Type {
-	default:
-		if entering {
-			io.WriteString(w, tags[node.Type][0])
-		} else {
-			io.WriteString(w, tags[node.Type][1])
+	case bf.Document:
+		return bf.GoToNext
+	case bf.Text:
+		w.Write(node.Literal)
+	case bf.Heading:
+		io.WriteString(w, "<")
+		if !entering {
+			io.WriteString(w, "/")
 		}
+		io.WriteString(w, "h"+strconv.Itoa(node.HeadingData.Level)+">")
+	case bf.Paragraph:
+		if entering {
+			io.WriteString(w, "<p>")
+		} else {
+			io.WriteString(w, "</p>")
+		}
+	case bf.CodeBlock:
+		fmt.Fprintf(w, "<pre><code class=\"language-%s\">%s</code></pre>", string(node.CodeBlockData.Info), node.Literal)
+	case bf.BlockQuote:
+		if entering {
+			io.WriteString(w, "<div class=\"well\">")
+		} else {
+			io.WriteString(w, "</div>")
+		}
+
+	default:
+		panic(node.Type.String())
 	}
 	if entering {
 		ident++
